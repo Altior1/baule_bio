5068c8213f99ea02845c83c1990d23ee250e9ce9
Altior1 <thomas.rud@outlook.fr>
2025-09-07 21:40:48 +0200

    ajout de credo

--- lib/baule_bio_web/components/layouts.ex
+++ lib/baule_bio_web/components/layouts.ex
@@ -37,26 +37,20 @@ defmodule BauleBioWeb.Layouts do
     ~H"""
     <header class="navbar px-4 sm:px-6 lg:px-8">
       <div class="flex-1">
-        <a href="/" class="flex-1 flex w-fit items-center gap-2">
-          <img src={~p"/images/logo.svg"} width="36" />
-          <span class="text-sm font-semibold">v{Application.spec(:phoenix, :vsn)}</span>
+        <a href={~p"/"} class="btn btn-ghost btn-sm rounded-btn">
+          <h1>Baule Bio</h1>
         </a>
       </div>
       <div class="flex-none">
         <ul class="flex flex-column px-1 space-x-4 items-center">
           <li>
-            <a href="https://phoenixframework.org/" class="btn btn-ghost">Website</a>
+            <a href={~p"/recettes"} class="btn btn-ghost">Lien vers les recettes</a>
           </li>
           <li>
-            <a href="https://github.com/phoenixframework/phoenix" class="btn btn-ghost">GitHub</a>
+            <a href={~p"/recettes/new"} class="btn btn-ghost">proposer une recettes</a>
           </li>
           <li>
             <.theme_toggle />
-          </li>
-          <li>
-            <a href="https://hexdocs.pm/phoenix/overview.html" class="btn btn-primary">
-              Get Started <span aria-hidden="true">&rarr;</span>
-            </a>
           </li>
         </ul>
       </div>
